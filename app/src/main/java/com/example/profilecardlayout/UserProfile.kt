9ab6dbb88cd988db4ce723f706fcf2bac4a013fc
Anas Ahmad <anasfarhad304@gmail.com>
2022-04-17 12:21:30 +0500

    Dynamic content commit

---
+++ app/src/main/java/com/example/profilecardlayout/UserProfile.kt
@@ -0,0 +1,37 @@
+package com.example.profilecardlayout
+
+data class UserProfile constructor(val name: String, val status: Boolean, val drawableImage: Int)
+
+
+// Mock List for testing layout
+val userProfileList = arrayListOf(
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+    UserProfile(name = "Marques Brownlee", status = false, drawableImage = R.drawable.mkbhd),
+    UserProfile(name = "Samantha", status = true, drawableImage = R.drawable.blonde),
+
+)
---
+++ app/src/main/java/com/example/profilecardlayout/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.example.profilecardlayout.ui.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val veryLightGrey = Color(0x60DCDCDC)
+
+val lightGreen200 = Color(0x9932CD32)
+
+/** Extenssion function for Custom Color */
+val Colors.lightGreen: Color
+    @Composable
+    get() = lightGreen200
+
+// another Extenssion function for custom colors
+val lightRed200 = Color(0xFFE66E66)
+val Colors.lightRed: Color
+    @Composable
+    get() = lightRed200
