12ac52b4b738e244dfb451d891119949ba9a7e7f
Anas <anasfarhad304@gmail.com>
2023-10-30 20:38:51 +0500

    name change

--- app/src/main/java/com/example/profilecardlayout/UserProfile.kt
+++ app/src/main/java/com/example/profilecardlayout/UserProfile.kt
@@ -11,7 +11,7 @@ data class UserProfile constructor(
 val userProfileList = arrayListOf(
     UserProfile(
         id = 0,
-        name = "Michaela Runnings",
+        name = "Gualim Agis",
         status = true,
         pictureUrl = "https://images.unsplash.com/photo-1485290334039-a3c69043e517?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
     ),
